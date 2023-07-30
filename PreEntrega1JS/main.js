d86cd4e4db4e118f312e300d758a0af9cc2c4a41
fernordfors <fernandonordfors@hotmail.com>
2023-07-30 15:02:45 -0300

    preentrega1js

---
+++ PreEntrega1JS/main.js
@@ -0,0 +1,22 @@
+/* let miFuncion = () => {
+    let inicioDeSesion = prompt(`Ingrese su usuario.`);
+    let contraseña = prompt(`Ingrese su Contraseña.`);
+    if(inicioDeSesion === "Ezequiel@123" && contraseña === "Ezequiel123"){
+    alert(`Hola, Ezequiel!`)
+    } else if(
+        alert(`El usuario y/o contraseña no son correctos.`)
+    );
+}
+miFuncion();  */
+
+
+
+/* const carritoDeCompra = () => {
+    const libros = [5000, 5500, 6000, 4000, 6500]; 
+    let total = 0;  
+    for (let i = 0; i < libros.length; i++){
+    total += libros[i]; }  
+    console.log(`El precio total de los productos es: ${total}`);
+} 
+carritoDeCompra(); */
+
